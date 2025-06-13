f2f24198b862bd5a4d573fbe7253482356338f56
custardvisuanetics <ich.ik.a.setsu.na@gmail.com>
2025-06-13 19:46:59 +0100

    feat: register & login

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from datetime import datetime
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
@@ -11,6 +12,7 @@ class User(db.Model):
     last_login = db.Column(db.DateTime)
 
 class Ticket(db.Model):
+    __tablename__ = 'tickets'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     description = db.Column(db.Text)
@@ -18,12 +20,13 @@ class Ticket(db.Model):
     priority = db.Column(db.String(20))
     status = db.Column(db.String(20), default='Open')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
-    assigned_to = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.Column(db.Integer, db.ForeignKey('users.id'))
+    assigned_to = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 class Comment(db.Model):
+    __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
-    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'))
+    ticket_id = db.Column(db.Integer, db.ForeignKey('tickets.id'))
     author = db.Column(db.String(100))
     content = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
